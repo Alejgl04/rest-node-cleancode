b168e6f945590e3ad5f88a50419ecd7f26b41567
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-11-08 06:41:09 -0500

    todo repository implement

---
+++ src/infrastructure/repository/todo.repository.impl.ts
@@ -0,0 +1,34 @@
+import { CreateTodoDto, TodoDatasource, TodoEntity, TodoRepository, UpdateTodoDto } from "../../domain";
+
+export class TodoRepositoryImpl implements TodoRepository {
+
+  constructor(
+    private readonly datasource: TodoDatasource
+  ){}
+  create(createTodoDto: CreateTodoDto): Promise<TodoEntity> {
+
+    return this.datasource.create(createTodoDto);
+
+  }
+  getAll(): Promise<TodoEntity[]> {
+
+    return this.datasource.getAll();
+
+  }
+  findById(id: number): Promise<TodoEntity> {
+
+    return this.datasource.findById(id);
+
+  }
+  update(updateTodoDto: UpdateTodoDto): Promise<TodoEntity> {
+
+    return this.datasource.update(updateTodoDto);
+
+  }
+  delete(id: number): Promise<TodoEntity> {
+
+    return this.datasource.delete(id);
+
+  }
+  
+}
