9111447a76b6d1efa5bda2b68a1b43502731f028
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-11-19 17:12:27 -0500

    index update

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,5 @@
-export * from './mongoDB/mongo-database';
 
-export * from './mongoDB/models/user.model'
+export * from './mongoDB/models/category.model';
+export * from './mongoDB/models/product.model';
+export * from './mongoDB/models/user.model';
+export * from './mongoDB/mongo-database';
