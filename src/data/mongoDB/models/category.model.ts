dafe7da0188475625ecc400b8475a7f65bd99c14
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-11-19 17:12:16 -0500

    category model

---
+++ src/data/mongoDB/models/category.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from 'mongoose';
+
+
+const categorySchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    require: [true, 'Name is required']
+  },
+
+  available: {
+    type: Boolean,
+    defaul: false,
+  },
+
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+
+
+});
+
+export const CategoryModel = mongoose.model('Category', categorySchema);
